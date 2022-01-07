c7762797a4b04970d1328a7a4f3ebd79f83aaf15
Fraser Waters <frassle@gmail.com>
2022-01-07 21:22:30 +0000

    Revert "Can infer ValueTuple from static type (#49)"
    This reverts commit f9446d082229da2d6d9b451adcd1bfce196d7903.
    
    This massivly slowed down tests such that they started timing out on github actions.
    Lets look at some perf improvments to bring those test times down first then loop back to this idea.

--- src/Pickler.Deserialize.cs
+++ src/Pickler.Deserialize.cs
@@ -1011,9 +1011,7 @@ namespace Ibasa.Pikala
                     throw new Exception($"Can not deserialize type '{type}', could not find expected field '{name}'");
                 }
 
-                // We always need to memo this value even if it looks like the field type is currently a ValueType. We could of changed the type from a reference type
-                // to a value type between serialisation and now. So when written out it might of used memos to refer to these objects and so we need to track them.
-                var deserInfo = new DeserializeInformation(typeof(object), true);
+                var deserInfo = new DeserializeInformation(typeof(object), !toSet.FieldType.IsValueType);
                 object? value = ReducePickle(Deserialize(state, deserInfo, genericTypeParameters, genericMethodParameters));
 
                 toSet.SetValue(uninitializedObject, value);
