20c4b39b16dddaca1730d29461986d866882df77
Fraser Waters <frassle@gmail.com>
2022-03-21 13:45:22 +0000

    format

--- src/AssemblyLoadContextExtensions.cs
+++ src/AssemblyLoadContextExtensions.cs
@@ -19,7 +19,7 @@ namespace Ibasa.Pikala
                 try
                 {
                     assemblies = System.Linq.Enumerable.ToArray(assemblyLoadContext.Assemblies);
-                } 
+                }
                 catch (AccessViolationException)
                 {
                     assemblies = null;
@@ -158,7 +158,7 @@ namespace Ibasa.Pikala
         /// <param name="access">The access rights of the assembly.</param>
         /// <returns>An object that represents the new assembly.</returns>
         public static AssemblyBuilder DefineDynamicAssembly(this AssemblyLoadContext assemblyLoadContext, AssemblyName name, AssemblyBuilderAccess access)
-        {            
+        {
             if (Environment.Version.Major >= 6)
             {
                 // For runtime 6.0 onwards we can set our AssemblyLoadContext as the current contextual context and then call DefineDynamicAssembly
