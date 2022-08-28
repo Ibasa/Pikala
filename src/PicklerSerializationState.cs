5cb339dce01420b81ff632c240cccecc8e235a6f
Fraser Waters <frassle@gmail.com>
2022-08-28 14:16:47 +0100

    Split serialization state source file

---
+++ src/PicklerSerializationState.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Ibasa.Pikala
+{
+    sealed class PicklerSerializationState
+    {
+        // This is built in from .NET 5 onwards.
+        class ReferenceEqualityComparer : EqualityComparer<object>
+        {
+            private static IEqualityComparer<object>? _defaultComparer;
+
+            public new static IEqualityComparer<object> Default
+            {
+                get { return _defaultComparer ?? (_defaultComparer = new ReferenceEqualityComparer()); }
+            }
+
+            public override bool Equals(object? x, object? y)
+            {
+                return ReferenceEquals(x, y);
+            }
+
+            public override int GetHashCode(object obj)
+            {
+                return System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
+            }
+        }
+
+        Dictionary<object, long> memo;
+        public BinaryWriter Writer { get; private set; }
+
+        public PicklerSerializationState(Stream stream)
+        {
+            memo = new Dictionary<object, long>(ReferenceEqualityComparer.Default);
+            Writer = new PickleWriter(new PickleStream(stream));
+        }
+        public bool MaybeWriteMemo(object value, byte? op)
+        {
+            if (memo.TryGetValue(value, out var offset))
+            {
+                if (op != null)
+                {
+                    Writer.Write(op.Value);
+                }
+                Writer.Write15BitEncodedLong(offset);
+                return true;
+            }
+            return false;
+        }
+
+        public void AddMemo(long position, object value)
+        {
+            // Save it in the memo for any later (or self) references
+            memo.Add(value, position);
+#if DEBUG
+            // In debug mode we do a sanity check that we haven't possibly screwed up memoisation by checking that every position stored in
+            // memo is unique
+            var set = new HashSet<long>(memo.Values);
+            System.Diagnostics.Debug.Assert(set.Count == memo.Count, "Two distinct objects tried to memoise to the same position");
+#endif
+        }
+
+        Stack<Action> trailers = new Stack<Action>();
+        List<Action> statics = new List<Action>();
+        int trailerDepth = 0;
+
+        public readonly HashSet<Type> SeenTypes = new HashSet<Type>();
+
+        public void CheckTrailers()
+        {
+            if (trailers.Count != 0)
+            {
+                throw new Exception("Serialization trailers count should of been zero");
+            }
+
+            foreach (var staticFieldWriter in statics)
+            {
+                staticFieldWriter();
+            }
+        }
+
+        public void RunWithTrailers(Action action)
+        {
+            int depth = trailerDepth++;
+
+            action();
+
+            if (depth == 0)
+            {
+                while (trailers.Count > 0)
+                {
+                    var trailer = trailers.Pop();
+                    trailer();
+                }
+            }
+
+            --trailerDepth;
+        }
+
+        public void PushTrailer(Action trailer, Action staticFields)
+        {
+            trailers.Push(trailer);
+            statics.Add(staticFields);
+        }
+    }
+}
