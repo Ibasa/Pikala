5621154c36a49647aac647d02072b520f8496a78
Fraser Waters <frassle@gmail.com>
2022-08-28 13:26:16 +0100

    Drop support for everything but net6.0 (#69)
    * Drop support for everything but net6.0
    
    * Fix lint
    
    * Remove some extension functions not-needed anymore

--- src/BinaryExtensions.cs
+++ src/BinaryExtensions.cs
@@ -4,46 +4,10 @@ using System.IO;
 namespace Ibasa.Pikala
 {
     /// <summary>
-    /// Couple of methods that are available in net5.0 but not in netstandard2.1.
+    /// Methods for BinaryWriter and BinaryReader for writing some non-standard formats.
     /// </summary>
-    static class NetStandard21Extensions
+    static class BinaryExtensions
     {
-        public static int Read7BitEncodedInt(this BinaryReader self)
-        {
-            uint result = 0;
-            byte byteReadJustNow;
-            const int MaxBytesWithoutOverflow = 4;
-            for (int shift = 0; shift < MaxBytesWithoutOverflow * 7; shift += 7)
-            {
-                byteReadJustNow = self.ReadByte();
-                result |= (byteReadJustNow & 0x7Fu) << shift;
-
-                if (byteReadJustNow <= 0x7Fu)
-                {
-                    return (int)result;
-                }
-            }
-            byteReadJustNow = self.ReadByte();
-            if (byteReadJustNow > 0b_1111u)
-            {
-                throw new FormatException("Too many bytes in what should have been a 7-bit encoded integer.");
-            }
-
-            result |= (uint)byteReadJustNow << (MaxBytesWithoutOverflow * 7);
-            return (int)result;
-        }
-
-        public static void Write7BitEncodedInt(this BinaryWriter self, int value)
-        {
-            uint v = (uint)value;
-            while (v > 0x7Fu)
-            {
-                self.Write((byte)(v | ~0x7Fu));
-                v >>= 7;
-            }
-            self.Write((byte)v);
-        }
-
         public static long Read15BitEncodedLong(this BinaryReader self)
         {
             ulong result = 0;
@@ -78,11 +42,6 @@ namespace Ibasa.Pikala
                 v >>= 15;
             }
             self.Write((ushort)v);
-        }
-
-        public static bool IsAssignableTo(this Type self, Type type)
-        {
-            return type.IsAssignableFrom(self);
         }
 
         public static void WriteNullableString(this BinaryWriter self, string? value)
